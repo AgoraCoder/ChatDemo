24886292ff5bd38dafc0cc5e56acfa226ecaf559
Ray <distinguished19kilo@gmail.com>
2016-06-21 23:55:02 -0500

    Add project files.

---
+++ ChatDemoSignalR/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace ChatDemoSignalR
+{
+    [HubName("chat")]
+    public class ChatHub : Hub
+    {
+        public void SendMessage(string message)
+        {
+            Clients.All.broadcastMessage(message);
+        }
+    }
+}
